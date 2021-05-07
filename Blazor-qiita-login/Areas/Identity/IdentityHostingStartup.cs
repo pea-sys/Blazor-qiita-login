436452039baab6542fbd0afafb9f8b8ee2398042
pea-sys <pea98258@gmail.com>
2021-05-07 21:25:27 +0900

    https://qiita.com/Mutz/items/1b0d7a826004dd9272db を参考にシンプルなログイン処理を実装

---
+++ Blazor-qiita-login/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Blazor_qiita_login.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Blazor_qiita_login.Areas.Identity.IdentityHostingStartup))]
+namespace Blazor_qiita_login.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                //services.AddDbContext<IdentityContext>(options =>
+                //    options.UseSqlServer(
+                //        context.Configuration.GetConnectionString("DefaultConnection")));
+                services.AddEntityFrameworkNpgsql().AddDbContext<IdentityContext>(opt =>
+                opt.UseNpgsql(context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
